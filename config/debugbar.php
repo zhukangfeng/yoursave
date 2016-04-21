34ec05ef1ac1449d4708c162d43074c82eef4225
zhu(shu) <zhukangfeng@users.noreply.github.com>
2016-04-21 18:53:16 +0900

    Update debugbar.php
    <feature-init : zhukangfeng>
    修改debugbar的显示内容，方便调试

--- config/debugbar.php
+++ config/debugbar.php
@@ -92,14 +92,14 @@ return array(
         'views'           => true,  // Views with their data
         'route'           => true,  // Current route information
         'laravel'         => false, // Laravel version and environment
-        'events'          => false, // All events fired
-        'default_request' => false, // Regular or special Symfony request logger
+        'events'          => true, // All events fired
+        'default_request' => true, // Regular or special Symfony request logger
         'symfony_request' => true,  // Only one can be enabled..
         'mail'            => true,  // Catch mail messages
-        'logs'            => false, // Add the latest log messages
-        'files'           => false, // Show the included files
-        'config'          => false, // Display config settings
-        'auth'            => false, // Display Laravel authentication status
+        'logs'            => true, // Add the latest log messages
+        'files'           => true, // Show the included files
+        'config'          => true, // Display config settings
+        'auth'            => true, // Display Laravel authentication status
         'session'         => true,  // Display session data
     ),
 
@@ -118,8 +118,8 @@ return array(
         ),
         'db' => array(
             'with_params'       => true,   // Render SQL with the parameters substituted
-            'timeline'          => false,  // Add the queries to the timeline
-            'backtrace'         => false,  // EXPERIMENTAL: Use a backtrace to find the origin of the query in your files.
+            'timeline'          => true,  // Add the queries to the timeline
+            'backtrace'         => true,  // EXPERIMENTAL: Use a backtrace to find the origin of the query in your files.
             'explain' => array(            // EXPERIMENTAL: Show EXPLAIN output on queries
                 'enabled' => false,
                 'types' => array('SELECT'), // array('SELECT', 'INSERT', 'UPDATE', 'DELETE'); for MySQL 5.6.3+
@@ -127,10 +127,10 @@ return array(
             'hints'             => true,    // Show hints for common mistakes
         ),
         'mail' => array(
-            'full_log' => false
+            'full_log' => true
         ),
         'views' => array(
-            'data' => false,    //Note: Can slow down the application, because the data can be quite large..
+            'data' => true,    //Note: Can slow down the application, because the data can be quite large..
         ),
         'route' => array(
             'label' => true  // show complete route on bar
