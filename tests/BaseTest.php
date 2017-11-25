1823c8448c052849d9987e9fcfa17fa4e511e6d0
zhukangfeng <zhukangfeng@uco.com>
2017-11-25 21:09:29 +0800

    <test : zhukangfeng> add test base file

---
+++ tests/BaseTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+/**
+ * based test class
+ *
+ */
+class BaseTest extends TestCase
+{
+
+}
